8f9fa475b143f15871201ebe9584cf04e1a95c19
LuisLinoRevilla <linorevilla@comunidad.unam.mx>
2026-04-09 08:28:17 -0600

    creacion de temas y validacion de campos

--- app/src/main/java/com/example/plantas/LoginFragment.kt
+++ app/src/main/java/com/example/plantas/LoginFragment.kt
@@ -6,32 +6,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.constraintlayout.motion.widget.TransitionBuilder.validate
 import androidx.navigation.fragment.findNavController
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLogingBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Aquí se dibuja tu diseño XML
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    // AQUÍ AGREGAREMOS LA LÓGICA DE LOS BOTONES
+    private fun setupValidation(){
+        binding.singInButton.isEnabled = false
+        binding.emailTied.addTextChangedListener{
+            validateFields()
+        }
+    }
+
+    private fun validateFields{
+        val email = binding.emailTied.text.toString().trim()
+        val password = binding.passwordTied.text.toString().trim()
+
+        val isEmailValid = isValidemail(email)
+        val idpassword = password.length >= 8
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 1. Buscamos los botones que pusimos en el XML usando sus IDs
         val btnRegister = view.findViewById<Button>(R.id.tvRegisterLink)
         val btnForgot = view.findViewById<Button>(R.id.tvForgotPass)
 
-        // 2. Acción para el botón de Registrarse
         btnRegister.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
 
-        // 3. Acción para el botón de Restablecer contraseña
         btnForgot.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_forgotPasswordFragment)
         }
--- app/src/main/java/com/example/plantas/RegisterFragment.kt
+++ app/src/main/java/com/example/plantas/RegisterFragment.kt
@@ -15,23 +15,21 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflamos el diseño de la pantalla de registro
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // 1. Buscamos la flecha de regresar y el botón verde de registrar
         val ivBack = view.findViewById<ImageView>(R.id.ivBack)
         val btnRegister = view.findViewById<Button>(R.id.btnRegister)
 
-        // 2. Acción para la flecha: Destruye esta pantalla y regresa al Login
+        //Acción para la flecha: Destruye esta pantalla y regresa al Login
         ivBack.setOnClickListener {
             findNavController().navigateUp()
         }
 
-        // 3. Acción para el botón verde: Nos lleva a la pantalla de información adicional
+
         btnRegister.setOnClickListener {
             findNavController().navigate(R.id.action_registerFragment_to_registerInfoFragment)
         }
