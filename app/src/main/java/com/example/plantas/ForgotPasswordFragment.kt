a3bc41ab39c4d4e6d5ce26c17c4b596da67ad4bc
LuisLinoRevilla <linorevilla@comunidad.unam.mx>
2026-04-13 20:44:22 -0600

    entrega primera parte

--- app/src/main/java/com/example/plantas/ForgotPasswordFragment.kt
+++ app/src/main/java/com/example/plantas/ForgotPasswordFragment.kt
@@ -5,25 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
+import com.example.plantas.databinding.FragmentForgotPasswordBinding
 
 class ForgotPasswordFragment : Fragment() {
+
+    private var _binding: FragmentForgotPasswordBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    ): View {
+        _binding = FragmentForgotPasswordBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val ivBack = view.findViewById<ImageView>(R.id.ivBackForgot)
+        setupValidation()
+        setupClickListeners()
+    }
 
-        ivBack.setOnClickListener {
+    private fun setupValidation() {
+        binding.btnRecoverPassword.isEnabled = false
+
+        binding.etRecoveryEmail.addTextChangedListener {
+            validateFields()
+        }
+    }
+
+    private fun validateFields() {
+        val email = binding.etRecoveryEmail.text.toString().trim()
+        val isEmailValid = isValidEmail(email)
+
+        binding.etRecoveryEmail.error = if (email.isEmpty() || isEmailValid) null else "Correo inválido"
+
+        binding.btnRecoverPassword.isEnabled = isEmailValid
+    }
+
+    private fun setupClickListeners() {
+        // Flecha para regresar
+        binding.ivBackForgot.setOnClickListener {
             findNavController().navigateUp()
         }
+
+        binding.btnRecoverPassword.setOnClickListener {
+            // Aquí en el futuro conectarás tu base de datos (ej. Firebase)
+            // Por ahora, al presionarlo, podemos simplemente regresar al Login
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
